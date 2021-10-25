4c4938e38a719f8f9e64b1284bc47d131e308b9f
jxia <jxia86@gmail.com>
2021-10-25 16:55:08 +0000

    part 2 rspec

--- rottenpotatoes/spec/part2_controller_spec.rb
+++ rottenpotatoes/spec/part2_controller_spec.rb
@@ -2,16 +2,22 @@ require 'rails_helper'
 
 describe MoviesController, type: :request do
     it "finds movies with same director" do
-        Movie.stub(:sameDirector).with('Star Wars').and_return(['Star Wars', 'THX-1138'])
-        expect(Movie.sameDirector('Star Wars')).to eq(['Star Wars', 'THX-1138'])
+        Movie.stub(:sameDirector).with('withSimilar').and_return(['Star Wars', 'THX-1138'])
+        expect(Movie.sameDirector('withSimilar')).to eq(['Star Wars', 'THX-1138'])
         #get '/search', params: { title: 'Star Wars' }
-        #I don't know why this doesnt work, if I do >rake routes I get
+        #I don't know why the above doesnt work, if I do ">rake routes" the route is there:
         #    search GET    /search/:title(.:format)   movies#search
         #but I always get a routing error with: No route matches [GET] "/search"
+        get '/search/withSimilar'
+        expect(response).to have_http_status(200)
+        expect(response.body).to include('Star Wars')
+        expect(response.body).to include('THX-1138')
     end
     
     it "does not find movies with same director" do 
-        Movie.stub(:sameDirector).with('Alien').and_return(nil) 
-        expect(Movie.sameDirector('Alien')).to eq(nil)
+        Movie.stub(:sameDirector).with('withoutSimilar').and_return(nil) 
+        expect(Movie.sameDirector('withoutSimilar')).to eq(nil)
+        get '/search/withoutSimilar'
+        expect(response).to redirect_to(root_url + "movies")
     end
 end
